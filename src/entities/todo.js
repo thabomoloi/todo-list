fedb38d23df607c834ddf57919d69dcf2d3d4306
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-11-13 17:51:01 +0200

    Add object constructor

---
+++ src/entities/todo.js
@@ -0,0 +1,9 @@
+function Todo(title, description, dueDate, priority, notes, checklist) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+}
+export default Todo;
