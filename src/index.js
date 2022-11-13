aa0dad97934b126277f59e468f14fdf8b7b5a913
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-11-13 22:40:58 +0200

    Add modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import loadHeader from "./view_layer/header";
 import loadSidebar from "./view_layer/sidebar";
+import loadAddTODO from "./view_layer/todo_modal";
 
 loadHeader();
 loadSidebar();
+loadAddTODO();
