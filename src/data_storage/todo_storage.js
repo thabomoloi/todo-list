268d5442b97be1ded053965c7c01615136a36192
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-11-14 13:25:22 +0200

    Add todo storage

---
+++ src/data_storage/todo_storage.js
@@ -0,0 +1,28 @@
+import Inbox from "../entities/inbox";
+import Project from "../entities/project";
+import Todo from "../entities/ToDo";
+import TodoList from "../entities/todoList";
+
+const TodoStorage = (() => {
+    const todo = localStorage.getItem("todoList");
+    var inbox;
+    var projects;
+    if (todo != null) {
+        const todoList = JSON.parse(todo);
+        inbox = todoList.inbox;
+        projects = todoList.projects;
+    }
+    else {
+        inbox = Inbox();
+        projects = [];
+    }
+
+    const getProjects = () => projects;
+    const getInbox = () => inbox;
+
+    const save = (todoList) => {
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+})();
+
+export default TodoStorage;
