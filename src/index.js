88d5a8f9ebab4e38492d4c6d1c3c33415e2d3107
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-11-14 18:13:46 +0200

    Instatiate and pass the inbox controller

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import loadHeader from "./view_layer/header";
 import loadSidebar from "./view_layer/sidebar";
 import loadAddTODO from "./view_layer/todo_modal";
 import loadInbox from "./view_layer/inbox";
+import InboxController from "./app_logic/inbox_controller"
 
-loadHeader();
-loadSidebar();
-loadAddTODO();
-loadInbox();
+
+function loadPage() {
+    const inboxController = new InboxController();
+    loadHeader();
+    loadSidebar();
+    loadAddTODO(inboxController);
+    loadInbox(inboxController);
+    inboxController.updateInbox();
+}
+loadPage();
