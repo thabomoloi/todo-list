85c5b7c27eb159379ee40b339dd5cd0a154a664c
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-11-14 20:58:16 +0200

    Add psedo-class and nbsp

--- src/view_layer/inbox.js
+++ src/view_layer/inbox.js
@@ -9,10 +9,11 @@ function loadInbox(inboxController) {
     // Inbox content
     const inbox = document.createElement("div");
     inbox.id = "inbox";
+    inbox.classList.add("task-view")
     content.appendChild(inbox);
 
     const addTaskBtn = document.createElement("button");
-    addTaskBtn.innerHTML = `<i class="fa-solid fa-plus"></i><span>Add task<span>`;
+    addTaskBtn.innerHTML = `<i class="fa-solid fa-plus">&nbsp;</i><span>Add task<span>`;
     addTaskBtn.className = "btn-add-task";
     content.appendChild(addTaskBtn);
 
