6d8136ad28369dad8cd9de7ba7ff2a97f2a744b6
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-11-14 12:37:54 +0200

    Update todo list object

--- src/entities/todoList.js
+++ src/entities/todoList.js
@@ -1,6 +1,5 @@
-function TodoList(anInbox, aProject, alist) {
+function TodoList(anInbox, theProjects) {
     const inbox = anInbox;
-    const project = aProject;
-    const taskList = alist;
-    return { inbox, project, taskList };
+    const projects = theProjects;
+    return {inbox, projects};
 }
