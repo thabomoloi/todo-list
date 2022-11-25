9697dad51a26a3427eebbed68b02560bc135676d
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-11-25 18:50:19 +0200

    Add project storage

---
+++ src/data_storage/ProjectStorage.js
@@ -0,0 +1,70 @@
+import TodoStorage from "./TodoStorage";
+import Project from "../entities/project";
+import Todo from "../entities/ToDo";
+
+class ProjectStorage {
+    constructor() {
+        this.todoStorage = new TodoStorage();
+        this.projects = this.todoStorage.getProjects();
+    }
+    getAllProjects() {
+        return this.projects;
+    }
+    addProject(id, name) {
+        this.projects.push(Project(id, name));
+        this.todoStorage.setProjects(this.projects);
+    }
+    updateProject(id, name, tasks) {
+        for (let i = 0; i < this.projects.length; i++) {
+            if (this.projects[i].id == id) {
+                this.projects[i].name = name;
+                this.projects[i].tasks = tasks;
+            }
+        }
+        this.todoStorage.setProjects(this.projects);
+    }
+    removeProject(id) {
+        const projs = this.projects.filter(item => item.id != id);
+        this.projects = projs;
+        this.todoStorage.setProjects(this.projects);
+    }
+    getAllProjectTasks(id) {
+        for (let i = 0; i < this.projects.length; i++) {
+            if (this.projects[i].id == id) {
+                return this.projects[i].tasks;
+            }
+        }
+        return [];
+    }
+    addProjectTask(projectID, taskID, title, description, dueDate, priority, done) {
+        for (let i = 0; i < this.projects.length; i++) {
+            if (this.projects[i].id == projectID) {
+                this.projects[i].tasks.push(Todo(taskID, title, description, dueDate, priority, done));
+            }
+        }
+        this.todoStorage.setProjects(this.projects);
+    }
+    updateProjectTask(projectID, taskID, title, description, dueDate, priority, done) {
+        for (let i = 0; i < this.projects.length; i++) {
+            if (this.projects[i].id == projectID) {
+                for (let j = 0; j < this.projects.tasks.length; j++) {
+                    if (this.projects[i].tasks[j].id == taskID) {
+                        this.projects[i].tasks[j] = Todo(taskID, title, description, dueDate, priority, done);
+                    }
+                }
+            }
+        }
+        this.todoStorage.setProjects(this.projects);
+    }
+    removeProjectTask(projectID, taskID) {
+        for (let i = 0; i < this.projects.length; i++) {
+            if (this.projects[i].id == projectID) {
+                const tasks = this.projects[i].tasks.filter(item => item.id != taskID);
+                this.projects[i].tasks = tasks;
+            }
+        }
+        this.todoStorage.setProjects(this.projects);
+    }
+}
+
+export default ProjectStorage;
