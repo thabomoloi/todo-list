d638624f748de9ba719e5b022b794faa5776285c
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-12-15 22:38:36 +0200

    Add header class

---
+++ src/View/Header.js
@@ -0,0 +1,34 @@
+class Header {
+
+    constructor() {
+        const header = document.querySelector("header");
+        if (header) {
+            header.innerHTML = `
+            <span></span>
+            <span class="site-name">ToDo List</span>
+            <button class="menu-button open-menu"><i class="fa-sharp fa-solid fa-bars"></i></button>
+            `;
+
+            const menuButton = document.querySelector("header .menu-button");
+            if (menuButton) {
+                const sidebar = document.querySelector("#sidemenu");
+                if (sidebar) {
+                    menuButton.addEventListener("click", event => {
+                        if (menuButton.classList.contains("open-menu")) {
+                            sidebar.style.display = "block";
+                            menuButton.classList.remove("open-menu");
+                            menuButton.classList.add("close-menu");
+                        } else {
+                            sidebar.style.display = "none";
+                            menuButton.classList.remove("close-menu");
+                            menuButton.classList.add("open-menu");
+                            sidebar.removeAttribute("style");
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
+
+export default Header;
