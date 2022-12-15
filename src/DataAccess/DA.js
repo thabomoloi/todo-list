ca5e9c9d5cc9e034367d6e0d5a42270b384de5c2
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-12-15 13:28:20 +0200

    Add data access class

---
+++ src/DataAccess/DA.js
@@ -0,0 +1,61 @@
+import Project from "../Entities/Project";
+import Task from "../Entities/Task";
+
+class DA {
+    constructor() {
+        this.todoList = localStorage.getItem("TODO");
+
+        const objects = [];
+        if (todoList)
+            objects = JSON.parse(this.todoList);
+
+        this.projects = [];
+
+        objects.forEach(project => {
+            const tasks = [];
+            project.tasks.forEach(task => {
+                tasks.push(new Task(task.ID, task.name, task.description, task.dueDate, task.priority, task.projectID))
+            });
+            this.projects.push(new Project(project.ID, project.name, project.tasks));
+        });
+    }
+
+    /**
+     * 
+     * @param {Project} project 
+     */
+    addProject(project) {
+        this.projects.push(project);
+        this.commit();
+    }
+    /**
+     * 
+     * @param {Project} project 
+     */
+    updateProject(project) {
+        for (let i = 0; i < this.projects.length; i++) {
+            if (project.ID == this.projects[i].ID) {
+                this.projects[i].name = project.name;
+                this.projects[i].tasks = project.tasks;
+                break;
+            }
+        }
+        this.commit();
+    }
+
+    /**
+     * 
+     * @param {string} ID 
+     */
+    removeProject(ID) {
+        const filter = this.projects.filter(project => ID != project.ID);
+        this.projects = filter;
+        this.commit();
+    }
+
+    commit() {
+        localStorage.setItem("TOD", JSON.stringify(this.projects));
+    }
+}
+
+export default DA;
