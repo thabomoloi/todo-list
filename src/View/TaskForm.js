fd596654b87ab880bf436e5c6f1d617d8a4d7349
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-12-22 13:12:35 +0200

    Add form methods

--- src/View/TaskForm.js
+++ src/View/TaskForm.js
@@ -1,14 +1,23 @@
+import ProjectController from "../AppLogic/ProjectController";
+import Task from "../Entities/Task";
+
 class TaskForm {
-    constructor(projectController, mode) {
-        mode = { ADD: 1, Edit: 2 };
+    /**
+     * 
+     * @param {ProjectController} projectController dd
+     */
+    constructor(projectController) {
+        this.mode = { ADD: 0, Edit: 1 };
         // Form's project controller
         this.projectController = projectController;
+        this.operation = 0;
 
         /******** Elements ************/
         // Modal Container
         this.container = document.createElement("div");
         this.container.id = "task-form";
         this.container.classList.add("modal-container");
+        this.load();
         // Modal
         this.modal = document.createElement("div");
         this.modal.classList.add("modal");
@@ -16,7 +25,7 @@ class TaskForm {
         // Content
         this.content = document.createElement("div");
         this.content.classList.add("modal-content");
-        this.content.innerHTML = (mode.ADD) ? "<h1>Add new task</h1>" : "<h1>Edit task</h1>";
+        this.content.innerHTML = `<h1 id="task-heading">Add new task</h1>`;
         this.modal.appendChild(this.content);
         // Form
         this.form = document.createElement("form");
@@ -25,16 +34,16 @@ class TaskForm {
 
         this.form.innerHTML = `
             <label for="task-name">Task name</label>
-            <input type="text" id="project-name" name="task-name" required>
+            <input type="text" id="task-name" required>
 
             <label for="task-description">Description <span class="optional">(Optional)</sapn></label>
-            <input type="text" id="task-description" name="task-description">
+            <input type="text" id="task-description">
 
             <label for="task-due-date">Due date</label>
-            <input type="date" id="task-due-date" name="task-due-date" required>
+            <input type="date" id="task-due-date" required>
 
             <label for="task-priority">Priority</label>
-            <select id="task-priority" name="task-priority" required>
+            <select id="task-priority" required>
                <option disabled selected></option>
                <option value="low">Low</option> 
                <option value="medium">Medium</option>
@@ -45,11 +54,66 @@ class TaskForm {
                 <button type="submit" id="submitButton">Submit</button>
             </div>
         `;
+        this.taskNameInput = document.querySelector("form input#task-name");
+        this.taskDescriptionInput = document.querySelector("form input#task-description");
+        this.taskDueDateInput = document.querySelector("form input#task-due-date");
+        this.taskPrioritySelect = document.querySelector("form select#task-priority");
 
+        this.addEventsListeners();
     }
     load() {
         const formSection = document.querySelector("#form-section");
         formSection.appendChild(this.container);
+    }
+    /**
+     * @param {number} operation 
+     * @param {Task} project
+     */
+    open(operation, task = null) {
+        this.task = task;
+        this.operation = operation;
+        const heading = document.querySelector("#task-form #task-heading");
+        heading.innerText = (operation == this.mode.ADD) ? "Add new task" : "Edit task";
+        this.container.style.display = "block";
+    }
+    close() {
+        this.clearFields();
+        this.container.style.display = "none";
+    }
+    clearFields() {
+        this.taskNameInput.value = "";
+        this.taskDescriptionInput.value = "";
+        this.taskDueDateInput.value = "";
+        this.taskPrioritySelect.value = "";
+    }
+    addEventsListeners() {
+        // Close modal when window is clicked
+        window.addEventListener("click", event => {
+            if (event.target == this.modal) {
+                this.close();
+            }
+        });
+
+        // Close modal on cancel
+        const cancelButton = document.querySelector("#task-form #cancelButton");
+        cancelButton.addEventListener("click", event => {
+            event.preventDefault();
+            this.close();
+        });
+
+        this.form.onsubmit = (event) => {
+            if (this.operation == this.mode.ADD)
+                this.projectController.addTask(this.taskNameInput.value, this.taskDescriptionInput.value, this.taskDueDateInput.value, this.taskPrioritySelect.value);
+            else {
+                this.task.name = this.taskNameInput.value;
+                this.task.description = this.taskDescriptionInput.value;
+                this.task.dueDate = this.taskDueDateInput.value;
+                this.task.priority = this.taskPrioritySelect.value;
+                this.projectController.updateTask(this.task);
+            }
+            this.close();
+        }
+
     }
 }
 
